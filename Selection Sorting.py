fca37f5a9d2438964e0137ef94f3bbb61dffa29a
Phatan Mohammad Imran Khan <35341727+itzmeimran@users.noreply.github.com>
2022-09-28 20:22:08 +0530

    Create Selection Sorting.py

---
+++ Selection Sorting.py
@@ -0,0 +1,18 @@
+class selectionsorting:
+    def descendingorder(list):
+        for i in range(len(list)):
+            maxvalue = max(list[i:])
+            maxindex = list.index(maxvalue)
+            list[i],list[maxindex]=list[maxindex],list[i]
+        print("Descending Order: ",list)
+    def ascendingorder(list):
+        for i in range(len(list)):
+            minvalue = min(list[i:])
+            minindex = list.index(minvalue)
+            list[i],list[minindex]=list[minindex],list[i]
+        print("Ascending Order : ", list)
+   
+a = selectionsorting
+list = [34,12,40,25,17]
+a.ascendingorder(list)
+a.descendingorder(list)
